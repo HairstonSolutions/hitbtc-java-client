a4d8d892158d9cf896769ca6b801263a3cf85b37
Jayson Hairston <JaysonH91@live.com>
2019-10-07 05:07:32 -0400

    Add Create order tests

--- src/test/java/com/hairstonsolutions/trading/clients/hitbtc/tests/TestCreateOrder.java
+++ src/test/java/com/hairstonsolutions/trading/clients/hitbtc/tests/TestCreateOrder.java
@@ -1,4 +1,33 @@
 package com.hairstonsolutions.trading.clients.hitbtc.tests;
 
+import com.hairstonsolutions.trading.clients.hitbtc.orders.CreateOrder;
+
+import com.hairstonsolutions.trading.clients.hitbtc.trades.TradeType;
+import org.junit.Test;
+
 public class TestCreateOrder {
+
+    @Test
+    public void testGetAllDefault() {
+        CreateOrder myCreateOrder = new CreateOrder();
+        System.out.println(myCreateOrder.getSymbol());
+        assert("BTCUSD".equals(myCreateOrder.getSymbol()));
+        assert(myCreateOrder.getSide().equals(CreateOrder.BUY));
+        assert(myCreateOrder.getTradeType().equals(TradeType.LIMIT));
+    }
+
+    @Test
+    public void testGeters() {
+        String mySymbol = "LTCBTC";
+        String buyside = "buy";
+        String tradeType = "limit";
+        String timeforce = "GTC";
+        CreateOrder myCreateOrder = new CreateOrder(mySymbol,buyside,tradeType);
+
+        assert(myCreateOrder.getSymbol().equals("LTCBTC"));
+        assert(myCreateOrder.getSide().equals("buy"));
+        assert(myCreateOrder.getTradeType().equals("limit"));
+
+    }
+
 }
