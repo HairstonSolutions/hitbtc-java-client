8a033180a438ea2b0f201374892f508ef0038174
Jayson Hairston <JaysonH91@live.com>
2019-10-19 11:28:52 -0400

    Add HitBTC Base URL, API Key, API Secret

---
+++ src/main/java/com/hairstonsolutions/trading/clients/hitbtc/api/HitBtcAPI.java
@@ -0,0 +1,61 @@
+package com.hairstonsolutions.trading.clients.hitbtc.api;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class HitBtcAPI {
+    public static final String BaseUrl = "https://api.hitbtc.com/api/2";
+
+    private String apiKey;
+    private String apiSecret;
+
+    public HitBtcAPI() {
+    }
+
+    public HitBtcAPI(String apiKey, String apiSecret) {
+        this.apiKey = apiKey;
+        this.apiSecret = apiSecret;
+    }
+
+    public HitBtcAPI(String configFile) {
+        loadKeysFromPropertiesFile(configFile);
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public String getApiSecret() {
+        return apiSecret;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    public void setApiSecret(String apiSecret) {
+        this.apiSecret = apiSecret;
+    }
+
+    @Override
+    public String toString() {
+        return apiKey+":"+apiSecret;
+    }
+
+    public void loadKeysFromPropertiesFile(String configFile) {
+        try (InputStream input = new FileInputStream(configFile)) {
+
+            Properties prop = new Properties();
+            prop.load(input);
+
+            this.apiKey = prop.getProperty("hitbtc.api.key");
+            this.apiSecret = prop.getProperty("hitbtc.api.secret");
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
---
+++ src/test/java/com/hairstonsolutions/trading/clients/hitbtc/tests/api/TestHitBtcAPI.java
@@ -0,0 +1,54 @@
+package com.hairstonsolutions.trading.clients.hitbtc.tests.api;
+
+import com.hairstonsolutions.trading.clients.hitbtc.api.HitBtcAPI;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class TestHitBtcAPI {
+
+    String configFile = "src/main/resources/hitbtckey.properties";
+    String testConfigFile = "src/test/resources/hitbtckey.properties";
+
+    String apiKey = "";
+    String secretKey = "";
+
+    @Before
+    public void LoadKeys() {
+
+        try (InputStream input = new FileInputStream(testConfigFile)) {
+
+            Properties prop = new Properties();
+            prop.load(input);
+
+            apiKey = prop.getProperty("hitbtc.api.key");
+            secretKey = prop.getProperty("hitbtc.api.secret");
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        System.out.println("ApiKey: "+apiKey);
+        System.out.println("SecretKey: "+secretKey);
+    }
+
+    @Test
+    public void makeAPIRequest() {
+        HitBtcAPI myhbc = new HitBtcAPI(apiKey,secretKey);
+        System.out.println("Base Url: "+ HitBtcAPI.BaseUrl);
+        System.out.println("Keys: "+ myhbc);
+    }
+
+    @Test
+    public void testLoadKeysFromPropertiesFile() {
+        HitBtcAPI myhbc = new HitBtcAPI(configFile);
+        System.out.println("Base Url: "+ HitBtcAPI.BaseUrl);
+        System.out.println("Keys: "+ myhbc);
+        assert (myhbc.getApiKey().equals("CHANGE-ME"));
+        assert (myhbc.getApiSecret().equals("CHANGE-ME"));
+    }
+}
