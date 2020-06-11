d2420502a8765200d306bcbe817a4c6f65bfdfb6
JaysonBH <JaysonH91@live.com>
2020-06-11 13:03:57 -0400

    Foundational Authorized ApiRequest Object for simplified API Requests

---
+++ src/main/java/com/hairstonsolutions/trading/clients/hitbtc/api/ApiAuthRequest.java
@@ -0,0 +1,76 @@
+package com.hairstonsolutions.trading.clients.hitbtc.api;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+public class ApiAuthRequest<T> {
+    private static final Log LOG = LogFactory.getLog(ApiAuthRequest.class);
+
+    private HitBtcAPI hitBtcAPI;
+    private HttpHeaders httpHeaders;
+
+    public ApiAuthRequest(HitBtcAPI hitBtcAPI) {
+        this.hitBtcAPI = hitBtcAPI;
+        this.httpHeaders = new HttpHeaders();
+        String encodedCredentials = hitBtcAPI.getEncodedCredentials();
+        httpHeaders.set("Authorization", "Basic " + encodedCredentials);
+    }
+
+    public Optional<T> getItemRequest(String requestURI, Class<T[]> responseType) {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpEntity<String> httpEntity = new HttpEntity<>(httpHeaders);
+
+        ResponseEntity<T[]> responseEntity;
+
+        try {
+            responseEntity = restTemplate.exchange(requestURI, HttpMethod.GET, httpEntity, responseType);
+        }
+        catch (Exception e) {
+            LOG.error(String.format("Error Retrieving request from API. API Response: %s", e.getMessage()));
+            return Optional.empty();
+        }
+
+        LOG.debug(String.format("Return Values: %s", responseEntity.toString()));
+
+        if (!responseEntity.hasBody())
+            return Optional.empty();
+
+        List<T> items = Arrays.asList(responseEntity.getBody());
+
+        return Optional.of(items.get(0));
+    }
+
+    public List<T> getListRequest(String requestURI, Class<T[]> responseType) {
+        List<T> items = new LinkedList<>();
+        RestTemplate restTemplate = new RestTemplate();
+        HttpEntity<String> httpEntity = new HttpEntity<>(httpHeaders);
+
+        ResponseEntity<T[]> responseEntity;
+
+        try {
+            responseEntity = restTemplate.exchange(requestURI, HttpMethod.GET, httpEntity, responseType);
+        }
+        catch (Exception e) {
+            LOG.error(String.format("Error Retrieving List request from API. API Response: %s", e.getMessage()));
+            return items;
+        }
+
+        LOG.debug(String.format("Return Values: %s", responseEntity.toString()));
+
+        if (!responseEntity.hasBody())
+            return items;
+
+        items = Arrays.asList(responseEntity.getBody());
+        return items;
+    }
+}
---
+++ src/test/java/com/hairstonsolutions/trading/clients/hitbtc/tests/api/ApiAuthRequestTest.java
@@ -0,0 +1,50 @@
+package com.hairstonsolutions.trading.clients.hitbtc.tests.api;
+
+import com.hairstonsolutions.trading.clients.hitbtc.api.ApiAuthRequest;
+import com.hairstonsolutions.trading.clients.hitbtc.api.HitBtcAPI;
+import com.hairstonsolutions.trading.clients.hitbtc.orders.Order;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ApiAuthRequestTest {
+    final String TESTCONFIGFILE = "src/test/resources/hitbtckey.properties";
+    private HitBtcAPI hitBtcAPI;
+
+    @Before
+    public void load() {
+        hitBtcAPI = new HitBtcAPI();
+        hitBtcAPI.loadKeysFromPropertiesFile(TESTCONFIGFILE);
+    }
+
+    @Test
+    public void singleItemAsOptionalHistoricalOrder() {
+        String clientOrderId = "84c5b2b6a1dc0ad12d257a5ce77cf58f";
+        String resourcePath = "/history/order";
+        String uri = HitBtcAPI.BASE_URL + resourcePath + "?clientOrderId=" + clientOrderId;
+
+        ApiAuthRequest apiAuthRequest;
+        apiAuthRequest = new ApiAuthRequest<Order>(hitBtcAPI);
+        Optional<Order> order = apiAuthRequest.getItemRequest(uri, Order[].class);
+
+        order.ifPresent(System.out::println);
+    }
+
+    @Test
+    public void listItemsAsHistoricalOrders() {
+        String resourcePath = "/history/order";
+        String uri = HitBtcAPI.BASE_URL + resourcePath;
+
+        ApiAuthRequest apiAuthRequest = new ApiAuthRequest<Order>(hitBtcAPI);
+        List<Order> historicalOrders = apiAuthRequest.getListRequest(uri, Order[].class);
+
+        int count = 0;
+        for (Order orders : historicalOrders) {
+            System.out.println(orders);
+            count++;
+        }
+        System.out.println(String.format("Orders Total: %s", count));
+    }
+}
