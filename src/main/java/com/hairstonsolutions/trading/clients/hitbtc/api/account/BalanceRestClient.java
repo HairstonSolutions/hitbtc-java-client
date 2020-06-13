a48286987c46c2bbab5712fa9c0a93e4cbe16b61
JaysonBH <JaysonH91@live.com>
2020-06-13 08:20:43 -0400

    Refactor BalanceRestClient with Authorized ApiRequest

--- src/main/java/com/hairstonsolutions/trading/clients/hitbtc/api/account/BalanceRestClient.java
+++ src/main/java/com/hairstonsolutions/trading/clients/hitbtc/api/account/BalanceRestClient.java
@@ -1,18 +1,13 @@
 package com.hairstonsolutions.trading.clients.hitbtc.api.account;
 
 import com.hairstonsolutions.trading.clients.hitbtc.account.Balance;
+import com.hairstonsolutions.trading.clients.hitbtc.api.ApiAuthRequest;
 import com.hairstonsolutions.trading.clients.hitbtc.api.HitBtcAPI;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
+import java.util.Optional;
 
 public class BalanceRestClient {
 
@@ -20,56 +15,23 @@ public class BalanceRestClient {
     private static final String REQUEST_URI = HitBtcAPI.BASE_URL + RESOURCE_PATH;
     private static final Log LOG = LogFactory.getLog(BalanceRestClient.class);
 
-    private HitBtcAPI hitBtcAPI;
-
-    public BalanceRestClient(HitBtcAPI hitBtcAPI) {
-        this.hitBtcAPI = hitBtcAPI;
-    }
-
-    public ResponseEntity<Balance[]> getForEntity() {
-        RestTemplate restTemplate = new RestTemplate();
-        String encodedCredentials = hitBtcAPI.getEncodedCredentials();
-        HttpHeaders headers = new HttpHeaders();
-
-        headers.set("Authorization", "Basic " + encodedCredentials);
-
-        HttpEntity<String> httpEntity = new HttpEntity<>(headers);
-
-        ResponseEntity<Balance[]> responseEntity = restTemplate.exchange(REQUEST_URI, HttpMethod.GET, httpEntity, Balance[].class);
-
-        LOG.info(String.format("Return Values: %s", responseEntity.toString()));
-
-        return responseEntity;
-    }
-
-    public List<Balance> getBalances() {
-        return retrieveBalances(hitBtcAPI);
-    }
-
     public static List<Balance> retrieveBalances(HitBtcAPI hitBtcAPI) {
-        RestTemplate restTemplate = new RestTemplate();
-        String encodedCredentials = hitBtcAPI.getEncodedCredentials();
-        HttpHeaders headers = new HttpHeaders();
+        ApiAuthRequest<Balance> apiAuthRequest = new ApiAuthRequest<>(hitBtcAPI);
+        List<Balance> balances = apiAuthRequest.getListRequest(REQUEST_URI,Balance[].class);
 
-        headers.set("Authorization", "Basic " + encodedCredentials);
+        if (balances.isEmpty())
+            LOG.error("There was an Error in retrieving Balances");
 
-        HttpEntity<String> httpEntity = new HttpEntity<>(headers);
-
-        ResponseEntity<Balance[]> responseEntity = restTemplate.exchange(REQUEST_URI, HttpMethod.GET, httpEntity, Balance[].class);
-
-        LOG.info(String.format("Return Values: %s", responseEntity.toString()));
-
-        return Arrays.asList(Objects.requireNonNull(responseEntity.getBody()));
+        return balances;
     }
 
-    public static Balance getBalance(HitBtcAPI hitBtcAPI, String currency) {
+    public static Optional<Balance> getBalance(HitBtcAPI hitBtcAPI, String currency) {
         List<Balance> balances = retrieveBalances(hitBtcAPI);
-        Balance selectedBalance = new Balance("USD", "0.0", "0.0");
 
-        for (Balance bal : balances) {
-            if (bal.getCurrency().equals(currency))
-                selectedBalance = bal;
-        }
-        return selectedBalance;
+        for (Balance singleBalance : balances)
+            if (singleBalance.getCurrency().equals(currency))
+                return Optional.of(singleBalance);
+
+        return Optional.empty();
     }
 }
--- src/test/java/com/hairstonsolutions/trading/clients/hitbtc/tests/api/account/TestBalanceRestClient.java
+++ src/test/java/com/hairstonsolutions/trading/clients/hitbtc/tests/api/account/TestBalanceRestClient.java
@@ -5,76 +5,42 @@ import com.hairstonsolutions.trading.clients.hitbtc.api.HitBtcAPI;
 import com.hairstonsolutions.trading.clients.hitbtc.api.account.BalanceRestClient;
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.http.ResponseEntity;
 
 import java.util.List;
+import java.util.Optional;
 
 public class TestBalanceRestClient {
 
-    final String TESTCONFIGFILE = "src/test/resources/hitbtckey.properties";
+    final String TEST_CONFIG_FILE = "src/test/resources/hitbtckey.properties";
     private HitBtcAPI hitBtcAPI;
 
     @Before
     public void load() {
         hitBtcAPI = new HitBtcAPI();
-        hitBtcAPI.loadKeysFromPropertiesFile(TESTCONFIGFILE);
-    }
-
-    @Test
-    public void testAuth() {
-        BalanceRestClient balanceRestClient = new BalanceRestClient(hitBtcAPI);
-
-        ResponseEntity<Balance[]> myGetBalanceResponseEntity = balanceRestClient.getForEntity();
-
-        assert myGetBalanceResponseEntity.getStatusCode().toString().equals("200 OK");
-
-        Balance[] balances = myGetBalanceResponseEntity.getBody();
-
-        assert balances != null;
-
-        for (Balance bal : balances) {
-            System.out.println(bal);
-        }
-
-        System.out.println(String.format("Balances Retrived: %s", balances.length));
+        hitBtcAPI.loadKeysFromPropertiesFile(TEST_CONFIG_FILE);
     }
 
     @Test
     public void getAllBalances() {
-        BalanceRestClient balanceRestClient = new BalanceRestClient(hitBtcAPI);
-        List<Balance> balances = balanceRestClient.getBalances();
-
-        for (Balance bal : balances) {
-            System.out.println(bal);
-        }
-
-        System.out.println(String.format("Balances Retrived: %s", balances.size()));
-    }
-
-    @Test
-    public void getAllBalancesStatic() {
         List<Balance> balances = BalanceRestClient.retrieveBalances(hitBtcAPI);
 
-        for (Balance bal : balances) {
-            System.out.println(bal);
-        }
-
-        System.out.println(String.format("Balances Retrived: %s", balances.size()));
+        System.out.println(balances);
+        System.out.println(String.format("Balances Retrieved: %s", balances.size()));
     }
 
     @Test
     public void getOneBalance() {
         String currency = "BTC";
-        Balance balance = BalanceRestClient.getBalance(hitBtcAPI, currency);
-        System.out.println(balance);
-        assert balance.getCurrency().equals(currency);
+        Optional<Balance> balance = BalanceRestClient.getBalance(hitBtcAPI, currency);
+        balance.ifPresent(System.out::println);
+        assert balance.isEmpty() || balance.get().getCurrency().equals(currency);
     }
 
     @Test
     public void getOneBalance2() {
-        String currency = "USDT";
-        Balance balance = BalanceRestClient.getBalance(hitBtcAPI, currency);
-        System.out.println(balance);
-        assert balance.getCurrency().equals("USD");
+        String currency = "USDC";
+        Optional<Balance> balance = BalanceRestClient.getBalance(hitBtcAPI, currency);
+        balance.ifPresent(System.out::println);
+        assert balance.isEmpty() || balance.get().getCurrency().equals(currency);
     }
 }
